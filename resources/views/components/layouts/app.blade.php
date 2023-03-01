a4573fe8adad248d3827fdaac1b1d5a5e4c16061
FortGH <fortjordi15@gmail.com>
2023-03-01 11:58:50 +0100

    Enlaces

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -47,10 +47,10 @@
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    <a href="https://github.com/FortGH/Laravel" class="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Contact</a>
+                    <a href="https://github.com/FortGH/Laravel" class="hover:underline">Contact</a>
                 </li>
             </ul>
         </div>
